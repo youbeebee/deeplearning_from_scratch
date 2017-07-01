4b3853c3ea1d5ecd72bcb48c3d0ca1af90c7f247
UBB <ubb@example.com>
2017-07-01 21:28:12 +0900

    init commit

---
+++ 1.5.numpy.py
@@ -0,0 +1,73 @@
+# 1.5.1 넘파이 가져오기
+import numpy as np
+
+# 1.5.2 넘파이 배열 생성하기
+x = np.array([1.0, 2.0, 3.0])
+print(x)  # [1. 2. 3.]
+print(type(x))  # <class 'numpy.ndarray'>
+
+# 1.5.3 넘파이의 산술 연산
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+print(x + y)  # 원소별 덧셈
+print(x - y)
+print(x * y)  # 원소별 곱셈(element-wise product)
+print(x / y)
+'''
+[ 3.  6.  9.]
+[-1. -2. -3.]
+[  2.   8.  18.]
+[ 0.5  0.5  0.5]
+'''
+print(x / 2.0)  # [ 0.5  1.   1.5]
+
+# 1.5.4 넘파이의 N차원 배열
+A = np.array([[1, 2], [3, 4]])
+print(A)
+'''
+[[1 2]
+ [3 4]]
+'''
+print(A.shape)  # 행렬의 형상 (2, 2)
+print(A.dtype)  # 행렬에 담긴 원소의 자료형 int64
+
+B = np.array([[3, 0], [0, 6]])
+print(A + B)
+print(A * B)
+print(A * 10)
+'''
+[[ 4  2]
+ [ 3 10]]
+[[ 3  0]
+ [ 0 24]]
+[[10 20]
+ [30 40]]
+'''
+
+# 1.5.5 브로드캐스트
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+print(A * B)
+'''
+[[10 40]
+ [30 80]]
+'''
+
+# 1.5.6 원소 접근
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+'''
+[[51 55]
+ [14 19]
+ [ 0  4]]
+ '''
+print(X[0])  # [51 55]
+print(X[0][1])  # 55
+for row in X:
+    print(row)
+
+X = X.flatten()  # X를 1차원 배열로 변환(평탄화)
+print(X)  # [51 55 14 19  0  4]
+print(X[np.array([0, 2, 4])])  # 인덱스가 0,2,4인 원소 얻기 [51 14  0]
+print(X > 15)  # [ True  True False  True False False]
+print(X[X > 15])  # [51 55 19]
---
+++ 1.6.matplotlib.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+# 1.6.1 단순한 그래프 그리기
+# 데이터 준비
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
+
+# 그래프 그리기
+# plt.plot(x, y)
+# plt.show()
+
+# 1.6.2 pyplot의 기능
+# 데이터 준비
+x = np.arange(0, 6, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# 그래프 그리기
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
+plt.xlabel("x")  # 축이름
+plt.ylabel("y")
+plt.title('sin & cos')
+plt.legend()
+plt.show()
+
+# 1.6.3 이미지 표시하기
+
+img = imread('./dataset/lena.png')
+
+plt.imshow(img)
+plt.show()
